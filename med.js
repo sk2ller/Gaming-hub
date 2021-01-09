8dfb49ce2c63628669cd7af24b920040fbdf8a3a
sk2ller <73031992+sk2ller@users.noreply.github.com>
2021-01-09 10:33:02 +0100

    Add files via upload

---
+++ med.js
@@ -0,0 +1,81 @@
+$(function(){
+    $('.hide-show').show();
+    $('.hide-show span').addClass('show')
+    
+    $('.hide-show span').click(function(){
+      if( $(this).hasClass('show') ) {
+        $(this).text('Hide');
+        $('input[name="login[password]"]').attr('type','text');
+        $(this).removeClass('show');
+      } else {
+         $(this).text('Show');
+         $('input[name="login[password]"]').attr('type','password');
+         $(this).addClass('show');
+      }
+    });
+    
+    $('form button[type="submit"]').on('click', function(){
+      $('.hide-show span').text('Show').addClass('show');
+      $('.hide-show').parent().find('input[name="login[password]"]').attr('type','password');
+    }); 
+  });
+ 
+  var hearts = document.querySelectorAll('.heart')
+
+for(heart of hearts){
+    heart.addEventListener('click',function(event){
+        if(event.target.classList.contains('far'))
+        event.target.classList.replace('far','fas')
+    else
+        event.target.classList.replace('fas','far')
+    })
+}
+var articles = document.querySelectorAll('article')
+
+function calSum(){
+    let sum = 0;
+    for(var i = 0; i < articles.length - 1; i++){
+        price = parseInt(articles[i].children[5].children[0].innerHTML)
+        sum += price
+    }
+
+    document.querySelector('#price').innerHTML = sum;
+}
+
+calSum();
+
+for(var i = 0; i < articles.length - 1; i++){
+    let qnt = articles[i].children[3];
+    let prx = articles[i].children[5].children[0];
+
+    articles[i].children[2].addEventListener('click', function(){
+        if(parseInt(qnt.innerHTML) > 0){
+            qnt.innerHTML = parseInt(qnt.innerHTML) - 1
+            prx.innerHTML = parseInt(qnt.innerHTML) * 100
+        }
+        calSum();
+    })
+    
+    articles[i].children[4].addEventListener('click', function(){
+        qnt.innerHTML = parseInt(qnt.innerHTML) + 1
+        prx.innerHTML = parseInt(qnt.innerHTML) * 100
+        calSum();
+    })
+
+    articles[i].children[6].addEventListener('click', function(e){
+        e.target.parentElement.remove();
+        articles = document.querySelectorAll('article')
+        calSum();
+        
+        if(articles.length == 1)
+            document.querySelector('h3').innerHTML = "Your shopping cart is empty!"
+    })
+
+    articles[i].children[7].addEventListener('click', function(e){
+        if(e.target.classList.contains('far'))
+            e.target.classList.replace('far','fas')
+        else
+            e.target.classList.replace('fas','far')
+            alert("Product has been added in wishlist.")
+    })
+}
